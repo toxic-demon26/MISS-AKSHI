83fd7ea81f02acfd0e2deb5ab4f15cd7d510c2c8
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-10-03 20:58:37 +0700

    Add files via upload

---
+++ LunaRobot/modules/repo.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+from LunaRobot import pbot as app
+from LunaRobot.utils.errors import capture_err
+from LunaRobot.utils.http import get
+
+
+@app.on_message(filters.command("repo") & ~filters.edited)
+@capture_err
+async def repo(_, message):
+    users = await get(
+        "https://api.github.com/repos/ridho17-ind/Flicks-Robot/contributors"
+    )
+    list_of_users = ""
+    count = 1
+    for user in users:
+        list_of_users += (
+            f"**{count}.** [{user['login']}]({user['html_url']})\n"
+        )
+        count += 1
+
+    text = f"""[Flicks Bot](https://t.me/FlicksManagerBot) | [Channel](https://t.me/SadRoomsInfo)
+**----------------
+| Contributors |
+----------------**
+{list_of_users}
+[❒ Repository ❒](https://github.com/ridho17-ind/Flicks-Robot)"""
+    await app.send_message(
+        message.chat.id, text=text, disable_web_page_preview=True
+    )
