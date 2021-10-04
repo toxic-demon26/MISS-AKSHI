2db2e0c585b6f8d0c74a382d8b127fbba6a95a08
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-10-04 19:10:37 +0700

    Update __aimultilanguage.py

--- LunaRobot/modules/__aimultilanguage.py
+++ LunaRobot/modules/__aimultilanguage.py
@@ -59,20 +59,20 @@ async def hmm(_, message):
         lel = await edit_or_reply(message, "`Processing...`")
         lol = add_chat(int(message.chat.id))
         if not lol:
-            await lel.edit("Flicks AI Already Activated In This Chat")
+            await lel.edit("Luna AI Already Activated In This Chat")
             return
         await lel.edit(
-            f"Flicks AI Successfully Added For Users In The Chat {message.chat.id}"
+            f"Luna AI Successfully Added For Users In The Chat {message.chat.id}"
         )
 
     elif status == "OFF" or status == "off" or status == "Off":
         lel = await edit_or_reply(message, "`Processing...`")
         Escobar = remove_chat(int(message.chat.id))
         if not Escobar:
-            await lel.edit("Flicks AI Was Not Activated In This Chat")
+            await lel.edit("Luna AI Was Not Activated In This Chat")
             return
         await lel.edit(
-            f"Flicks AI Successfully Deactivated For Users In The Chat {message.chat.id}"
+            f"Luna AI Successfully Deactivated For Users In The Chat {message.chat.id}"
         )
 
     elif status == "EN" or status == "en" or status == "english":
