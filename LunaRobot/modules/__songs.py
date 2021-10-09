0baed7aaff0493787cd31cbf94d558283c641a94
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 16:05:14 +0530

    Update __songs.py

--- LunaRobot/modules/__songs.py
+++ LunaRobot/modules/__songs.py
@@ -146,7 +146,7 @@ async def deezer(_, message):
     await m.delete()
     
     
-__mod_name__ = "sᴏɴɢs"
+__mod_name__ = "Songs🎵"
 
 __help__ = """
 • `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
