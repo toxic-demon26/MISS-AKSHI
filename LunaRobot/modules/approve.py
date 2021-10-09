1fba091a115544a94d51f1ed54ed9f892e3a5c1c
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 16:25:16 +0530

    Update approve.py

--- LunaRobot/modules/approve.py
+++ LunaRobot/modules/approve.py
@@ -221,6 +221,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "ᴀᴘᴘʀᴏᴠᴀʟs"
+__mod_name__ = "Approvals👍"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
