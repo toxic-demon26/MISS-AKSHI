b5c25db169201f1b37d881f4617a5219b379fbe2
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 16:59:52 +0530

    Update shell.py

--- LunaRobot/modules/shell.py
+++ LunaRobot/modules/shell.py
@@ -45,6 +45,6 @@ def shell(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh"], shell)
 dispatcher.add_handler(SHELL_HANDLER)
-__mod_name__ = "sʜᴇʟʟ"
+__mod_name__ = "Shell🔑"
 __command_list__ = ["sh"]
 __handlers__ = [SHELL_HANDLER]
