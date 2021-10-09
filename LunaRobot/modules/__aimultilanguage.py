2031664dc6741204c75a37a1d0f4c536f83796a7
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 15:53:22 +0530

    Update __aimultilanguage.py

--- LunaRobot/modules/__aimultilanguage.py
+++ LunaRobot/modules/__aimultilanguage.py
@@ -355,7 +355,7 @@ async def inuka(client, message):
 
 
 __help__ = """
-<b> Chatbot </b>
+<b> Chatbot🤖 </b>
 Livvy AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
  - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
