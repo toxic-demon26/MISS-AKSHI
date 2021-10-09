12fe1129b0a8d438d8cd430b9500d88e4ada99e8
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 16:57:51 +0530

    Update rules.py

--- LunaRobot/modules/rules.py
+++ LunaRobot/modules/rules.py
@@ -125,7 +125,7 @@ __help__ = """
   /clearrules*:* clear the rules for this chat.
 """
 
-__mod_name__ = "ʀᴜʟᴇs"
+__mod_name__ = "Rules📜"
 
 GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
 SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
