d665067c6369d455730afa3a5c1c5388e1b4596c
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 15:39:27 +0530

    Update alive.py

--- LunaRobot/modules/alive.py
+++ LunaRobot/modules/alive.py
@@ -4,25 +4,11 @@ import os
 from telethon import events, Button
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from LunaRobot.events import register as MEMEK
+from LunaRobot.events import register as LIVVYCMD
 from LunaRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/88f82e4620245cb398df7.jpg"
-
-@MEMEK(pattern=("/alive"))
-async def awake(event):
-  tai = event.sender.first_name
-  LUNA = "**Holla I'm Luna!** \n\n"
-  LUNA += "🔴 **I'm Working Properly** \n\n"
-  LUNA += "🔴 **My Master : [zeinzo](https://t.me/tdrki_1)** \n\n"
-  LUNA += f"🔴 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/lunatapibot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/lunasupportgroup")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
-
-@MEMEK(pattern=("/reload"))
+@LIVVYCMD(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
-  text = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
+  text = "✅ **Bot Successfully Updated**"
   await tbot.send_message(event.chat_id, text)
