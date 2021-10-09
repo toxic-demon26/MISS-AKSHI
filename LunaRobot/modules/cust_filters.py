93af1563e55701564418313c6db4fabe088535c9
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-09 16:35:51 +0530

    Update cust_filters.py

--- LunaRobot/modules/cust_filters.py
+++ LunaRobot/modules/cust_filters.py
@@ -639,7 +639,7 @@ Check  /markdownhelp to know more!
 
 """
 
-__mod_name__ = "ғɪʟᴛᴇʀs"
+__mod_name__ = "Filters🔥"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
