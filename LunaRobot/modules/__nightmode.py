86e1c8bc35cbf6d5185cc284b080a8056a862b34
vaibhavchandra12 <77480184+vaibhavchandra12@users.noreply.github.com>
2021-10-11 23:36:42 +0530

    Update __nightmode.py

--- LunaRobot/modules/__nightmode.py
+++ LunaRobot/modules/__nightmode.py
@@ -129,7 +129,7 @@ async def job_close():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Rizky**"
+              int(pro.chat_id), "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By Livvy**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered by rizky**"
+              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered by Livvy**"
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
