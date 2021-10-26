dd068a6dddcc11fbc096ebb664b16c24cd99f886
sᴀᴅᴇᴡ ᴊᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-10-26 11:20:14 +0530

    Update mongo.py

--- LunaRobot/mongo.py
+++ LunaRobot/mongo.py
@@ -5,19 +5,19 @@ from motor import motor_asyncio
 from LunaRobot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from LunaRobot.conf import get_int_key, get_str_key
+from LunaRobot.config import get_int_key, get_str_key
 
 
-MONGO_PORT = get_int_key("27017")
+MONGO_PORT = get_int_key("27018")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "LivvyBot"
+MONGO_DB = "LunaRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["misslivvybot"]
+db = client["LunaRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
